150f085db7f3c94068f9ffefcdb7d8dac7f2d5f2
Zach <zachchristensen28@gmail.com>
2017-08-23 17:49:15 -0600

    updated

--- navjs.js
+++ navjs.js
@@ -1,6 +1,6 @@
-$(function(){
-    $('.icon').on('click', function(){
-        $('nav').toggleClass('responsive');
-        console.log('clicked');
-    });
+$(function () {
+  $('.icon').on('click', function () {
+    $('#no-show').slideToggle('slow');
+
+  });
 });
